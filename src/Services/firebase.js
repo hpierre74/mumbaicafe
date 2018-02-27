d1c0cf541566cc715196b900bc9c314c2c5aab52
hpierre74 <pierrehuyghe.pro@gmail.com>
2018-02-27 12:55:27 +0100

    gitignore fix

--- src/Services/firebase.js
+++ src/Services/firebase.js
@@ -1,15 +1,7 @@
 import * as firebase from 'firebase';
 import '../firebase.conf.js';
 
-// const config = {
-//     apiKey: "AIzaSyC7nS-QSgNY_ug38x3A-KnDZqTxeneV-Zw",
-//     authDomain: "mumbai-react.firebaseapp.com",
-//     databaseURL: "https://mumbai-react.firebaseio.com",
-//     projectId: "mumbai-react",
-//     storageBucket: "mumbai-react.appspot.com",
-//     messagingSenderId: "280498275819"
-// };
-// const db_init  = firebase.initializeApp(config);
+
 const db = firebase.database();
 
 class FirebaseService {
